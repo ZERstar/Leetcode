96aaac204d0c2590696cc25571f4f4754a59c161
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-01-03 20:54:22 +0530

    Time: 163 ms (7.87%), Space: 12.2 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,7 +4,7 @@ class Solution {
     const int n = A[0].length();
     int ans = 0;
 
-    for (int j = 0; j < n; ++j)
+    for (int j = 0; j < n; j++)
       for (int i = 0; i + 1 < A.size(); i++)
         if (A[i][j] > A[i + 1][j]) {
           ans++;
