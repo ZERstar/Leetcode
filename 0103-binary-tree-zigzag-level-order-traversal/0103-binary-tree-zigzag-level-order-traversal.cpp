7a42aa03d2a290c2d634a4f25328ea768d1b14f3
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-03-21 01:48:54 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (49.68%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -10,32 +10,36 @@
  * };
  */
 class Solution {
- public:
-  vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-    if (root == nullptr)
-      return {};
-
-    vector<vector<int>> ans;
-    queue<TreeNode*> q{{root}};
-    bool isLeftToRight = true;
-
-    while (!q.empty()) {
-      const int size = q.size();
-      vector<int> currLevel(size);
-      for (int i = 0; i < size; ++i) {
-        TreeNode* node = q.front();
-        q.pop();
-        const int index = isLeftToRight ? i : size - i - 1;
-        currLevel[index] = node->val;
-        if (node->left)
-          q.push(node->left);
-        if (node->right)
-          q.push(node->right);
-      }
-      ans.push_back(currLevel);
-      isLeftToRight = !isLeftToRight;
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root==NULL){
+            return result;
+        }
+        
+        queue<TreeNode*>nodesQueue;
+        nodesQueue.push(root);
+        bool leftToRight = true;
+        
+        while(!nodesQueue.empty()){
+            int size = nodesQueue.size();
+            vector<int> row(size);
+            for(int i=0;i<size;i++){
+                TreeNode* node = nodesQueue.front();
+                nodesQueue.pop();
+                
+                int index = leftToRight ? i : (size-1-i);
+                row[index] = node->val;
+                if(node->left){
+                    nodesQueue.push(node->left);
+                }
+                if(node->right){
+                    nodesQueue.push(node->right);
+                }
+            }
+            leftToRight = !leftToRight;
+            result.push_back(row);
+        }
+        return result;
     }
-
-    return ans;
-  }
 };
