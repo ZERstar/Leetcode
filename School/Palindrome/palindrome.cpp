11c7bb58e70468a5877fd1ada19067e698a7c53c
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-05-18 12:31:11 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindrome/palindrome.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    string num = to_string(n);
+		    int left = 0;
+		    int right = num.length()-1;
+		    while(left<=right){
+		        if(num[left]!=num[right]) return "No";
+		        left++;
+		        right--;
+		    }
+		    return "Yes";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
