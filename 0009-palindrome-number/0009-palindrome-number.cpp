644a2d1eaa442994d547ce7d82bcbd3bf16e9e19
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-05-18 12:25:17 +0530

    Time: 21 ms (31.22%), Space: 5.9 MB (85.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string num = to_string(x);
+        int left = 0, right = num.size()-1;
+        while(left<=right){
+            if(num[left]!=num[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
