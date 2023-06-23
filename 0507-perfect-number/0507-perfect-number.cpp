f027153964ac2b728134dcfbceeb3198b09cb9a3
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-06-24 03:32:40 +0530

    Time: 4 ms (79.12%), Space: 6 MB (15.95%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num==1) return false;
+        int sum=0;
+        
+        for(int i=1;i<=sqrt(num);i++){
+            if(num%i==0) sum += num/i + i;
+        }
+        // cout<<sum;
+        if(sum/2 == num) return true;
+        else return false;
+    }
+};
