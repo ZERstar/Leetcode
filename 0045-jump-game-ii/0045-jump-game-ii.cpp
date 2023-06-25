736804465550e94d28c2e306f133fab8380d97bd
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-06-25 05:44:02 +0530

    Time: 16 ms (79.52%), Space: 16.5 MB (96.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int jump=0, maxSoFar=0, currentMax=0;
-        for(int i=0;i<nums.size()-1;i++){
-            maxSoFar = max(maxSoFar, i+nums[i]);
-            if(i==currentMax){
-                jump++;
-                currentMax=maxSoFar;
+    int jump(vector<int>& arr) {
+        int n = arr.size();
+        int maxReach = arr[0];
+        int stepsLeft = arr[0];
+        int jumps = 1;
+        
+        if(n<=1) return 0;
+        else if(arr[0]==0) return -1;
+        
+        int i=1;
+        while(i<n){
+            if(i==n-1) return jumps;
+            maxReach = max(maxReach, i+arr[i]);
+            stepsLeft--;
+            
+            if(stepsLeft==0){
+                jumps++;
+                if(maxReach==i){
+                    return -1;
+                }
+                stepsLeft=maxReach-i;
             }
+            i++;
         }
-        return jump;
+        return jumps;
     }
 };
