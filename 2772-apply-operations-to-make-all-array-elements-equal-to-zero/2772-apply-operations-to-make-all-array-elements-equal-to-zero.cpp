85437dba3be0d9f712ce3497185db7ff127b4d6f
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-07-11 18:50:43 +0530

    Time: 174 ms (80.40%), Space: 94 MB (90.09%) - LeetHub

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkArray(vector<int>& nums, int k) {
+        int sum=0, n=nums.size();
+        for(int i=0;i<n;i++){
+            if(sum > nums[i]) return false;
+            
+            nums[i] -= sum;
+            sum += nums[i];
+            
+            if(i >= k - 1) sum -= nums[i - k + 1];
+        }
+        return sum == 0;
+    }
+};
