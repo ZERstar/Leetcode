440b682c5821c535bc7a4889b553ccb39c6c92b9
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-07-15 13:12:49 +0530

    Time: 7 ms (97.14%), Space: 12.3 MB (91.79%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int countKDifference(vector<int>& nums, int k) {
         int ans=0;
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(abs(nums[i]-nums[j])==k) ans++;
-            }
+        int hash[101] = {0};
+        for(int num : nums){
+            hash[num]++;
+        }
+        for(int i=k+1;i<=100;i++){
+            ans += (hash[i] * hash[i-k]);
         }
         return ans;
     }
