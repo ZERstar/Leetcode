8eb22eb8570b72a2980f7ac09dd3bfa51fa33057
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-07-21 16:35:06 +0530

    Time: 202 ms (58.32%), Space: 27.2 MB (45.29%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int target =0;
-        sort(nums.begin(),nums.end());
-        set<vector<int>>s;
-        vector<vector<int>>ans;
-        for(int i=0;i<nums.size();i++){
-            int j=i+1;
-            int k = nums.size()-1;
-            while(j<k){
-                int sum= nums[i]+nums[j]+nums[k];
-                if(sum==0){ s.insert({nums[i],nums[j],nums[k]});
-                j++;
-                k--;}
-                else if(sum<target) j++;
-                else k--;
+    vector<vector<int>> result;
+    void twoSum(vector<int>&nums, int target, int i, int j){
+        while(i<j){
+            if(nums[i] + nums[j] < target) i++;
+            else if(nums[i] + nums[j] > target) j--;
+            else{
+                while(i<j && nums[i] == nums[i+1]) i++;
+                while(i<j && nums[j] == nums[j-1]) j--;
+                result.push_back({-target, nums[i], nums[j]});
+                i++;
+                j--;
             }
         }
-        for(auto it : s) ans.push_back(it);
-        return ans;
+    }
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 3) return {};
+        
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            
+            int n1 = nums[i];
+            int target = -n1;
+            
+            twoSum(nums, target, i+1, n-1);
+        }
+        return result;
     }
 };
