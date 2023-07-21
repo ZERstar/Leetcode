8eb6d81cc092b6acac55748e3cf38d48077dfa23
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-07-21 16:35:36 +0530

    Time: 197 ms (59.87%), Space: 27.2 MB (45.11%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -20,7 +20,7 @@ public:
         if(n < 3) return {};
         
         sort(nums.begin(), nums.end());
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             
             int n1 = nums[i];
