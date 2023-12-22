f0a9e7273e94434d2bfe7a663b2f83efeafac62b
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-12-22 18:29:47 +0530

    Time: 3 ms (66.04%), Space: 11.4 MB (34.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l=0;
-        int r=n-1;
-         while(l<=r){
-             int mid=(l+r)/2;
-            if(nums[mid]==target) {return mid;}
-            else if(nums[l] <= nums[mid]){
-                if(nums[l] <= target && target <= nums[mid]){
-                    r = mid -1;
-                }
-                else { l = mid + 1;}
+    
+    int findPivot(vector<int>& nums, int n){
+        int l=0,r=n-1;
+        
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] > nums[r]){
+                l = mid + 1;
             }
             else{
-                if(nums[mid] <= target && target <= nums[r]){
-                    l = mid + 1;
-                }
-                else {r = mid - 1;}
+                r = mid;
             }
         }
-        return -1;
+        return r;
+    }
+    
+    int binarySearch(int l, int r, vector<int>& nums, int target){
+        int idx = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                idx = mid;
+                break;
+            }
+            else if(nums[mid] < target){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1;
+            }
+        }
+        return idx;
+    }
+    
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        int pivot_index = findPivot(nums, n);
+        
+        int idx = binarySearch(0, pivot_index - 1, nums, target);
+        
+        if(idx != -1){
+            return idx;
+        }
+        
+        idx = binarySearch(pivot_index, n - 1, nums, target);
+        
+        return idx;
     }
 };
