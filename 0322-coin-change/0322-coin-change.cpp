95f25b3e878552d2af56aeeb29317e9ab08542d4
Tejas <73245011+ZERstar@users.noreply.github.com>
2023-12-28 18:17:28 +0530

    Time: 183 ms (9.91%), Space: 15.7 MB (47.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-    int n = coins.size();
-
-    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
-    dp[0][0] = 0;
-
-    for (int i = 1; i <= n; i++) {
-        dp[i][0] = 0;
-        for (int j = 1; j <= amount; j++) {
-            int coinValue = coins[i - 1];
-            if (j >= coinValue && dp[i][j - coinValue] != INT_MAX) {
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - coinValue] + 1);
-            } else {
-                dp[i][j] = dp[i - 1][j];
+    int helper(int n, vector<int>& coins, int m, vector<int>& dp){
+        if(n==0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        int ans = INT_MAX;
+        for(int i=0;i<m;i++){
+            if(n-coins[i]>=0){
+                int subProb = helper(n-coins[i], coins, m, dp);
+                if (subProb != INT_MAX) {
+                    ans = min(ans, subProb + 1);
+                }
             }
         }
+        return dp[n] = ans;
     }
-
-    return (dp[n][amount] == INT_MAX) ? -1 : dp[n][amount];
-}
-
+    
+    
+    int coinChange(vector<int>& coins, int amount) {
+        int m = coins.size();
+        vector<int> dp(amount+1,-1);
+        int result = helper(amount, coins, m, dp);
+        return (result == INT_MAX) ? -1 : result;
+    }
 };
