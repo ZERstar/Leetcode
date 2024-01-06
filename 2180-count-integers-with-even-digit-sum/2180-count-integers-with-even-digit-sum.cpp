10e1af65413bc181842366c37572418a8003221f
Tejas <73245011+ZERstar@users.noreply.github.com>
2024-01-06 23:16:25 +0530

    Time: 3 ms (15.91%), Space: 6.5 MB (15.48%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countEven(int num) {
+        int ans=0;
+        for(int i=2;i<=num;i++){
+            int sum = 0;
+            int k = i;
+            while(k>0){
+                int g = k%10;
+                sum+=g;
+                k/=10;
+            }
+            if(sum%2==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
